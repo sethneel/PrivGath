df6cedde9d3682cbb5b1359430825e621c0e1dec
sethneel <sethneel93@gmail.com>
2018-01-28 22:01:59 -0500

    priv ucb stochastic

--- bandits.py
+++ bandits.py
@@ -7,27 +7,28 @@ an implementation of the counter mechanism https://eprint.iacr.org/2010/076.pdf
 """
 
 
-def private_counter(T, epsilon):
+def private_counter(k, T, epsilon, sensitivity=2):
     """Returns array of T representing sum of laplace noise added to means in epsilon d.p. private counter"""
-
-    priv_noise = [0.0]*T
-    eps_prime = epsilon/np.log2(T)
-    digits = int(np.ceil(np.log2(T)))
-    alpha_array = [0]*digits
-    for j in range(1, T+1):
-        """Update noise, stored in priv_noise"""
-        # Get the binary expansion of j
-        bin_j = '{0:b}'.format(j)
-        # Get the first non-zero bit
-        i = get_min_dex(bin_j)
-        # Set all other alpha j < i to 0
-        for l in range(i):
-            alpha_array[l] = 0
-        alpha_array[i] = np.random.laplace(loc=0, scale=np.log2(T)/eps_prime)  # Laplace noise
-        # Noise added to the jth sum is the sum of all of the alphas with nonzero binary representation
-        priv_noise[j-1] = np.sum([alpha_array[k] for k in range(min(digits, len(bin_j))) if bin_j[k] == '1'])
-
-    return priv_noise
+    priv_noises = dict((u, []) for u in range(k))
+    for t in range(k):
+        priv_noise = [0.0]*T
+        eps_prime = epsilon/np.log2(T)
+        digits = int(np.ceil(np.log2(T)))
+        alpha_array = [0]*digits
+        for j in range(1, T+1):
+            """Update noise, stored in priv_noise"""
+            # Get the binary expansion of j
+            bin_j = '{0:b}'.format(j)
+            # Get the first non-zero bit
+            i = get_min_dex(bin_j)
+            # Set all other alpha j < i to 0
+            for l in range(i):
+                alpha_array[l] = 0
+            alpha_array[i] = np.random.laplace(loc=0, scale=sensitivity*np.log2(T)/eps_prime)  # Laplace noise
+            # Noise added to the jth sum is the sum of all of the alphas with nonzero binary representation
+            priv_noise[j-1] = np.sum([alpha_array[k] for k in range(min(digits, len(bin_j))) if bin_j[k] == '1'])
+        priv_noises[t] = priv_noise
+    return priv_noises
 
 
 def get_min_dex(binary_string):
@@ -71,7 +72,7 @@ def get_means(gap=.1):
 
 
 def get_sample(mu):
-    """Return sample from truncated normal in [-1,1] with mean mu."""
+    """Return sample from truncated normal in [0,1] with mean mu."""
 
     # sample truncated normal in [-mu, 1-mu]
     mean_s = truncnorm.mean(-mu, 1-mu, 0, 1)
@@ -79,6 +80,17 @@ def get_sample(mu):
     # get mean mu
     s = s - mean_s + mu
     return s
+
+
+def get_priv_ucb(delta, history, priv_noises, T, epsilon):
+    if history is None:
+        return None
+    K = len(history.keys())
+    gamma = K*np.power(np.log(T), 2)*np.log(K*T*np.log(T)*1.0/delta)*1.0/epsilon
+    noisy_means = [(history[i][0] + priv_noises[i][int(history[i][1])])/history[i][1] for i in range(K)]
+    ucb_list = [noisy_means[i] + np.sqrt(2*np.log(1/delta)/history[i][1]) + gamma/history[i][1] for i in range(K)]
+    ucb = np.argmax(ucb_list)
+    return ucb
 
 
 def ucb_bandit_run(time_horizon=500, gap=.1):
@@ -102,10 +114,26 @@ def ucb_bandit_run(time_horizon=500, gap=.1):
     return history
 
 
-def priv_ucb_bandit_run(time_horizon=500, gap=.1, epsilon=.1):
+def priv_ucb_bandit_run(time_horizon=500, delta=.95, gap=.1, epsilon=.1):
     """"Run epsilon-Private UCB algorithm w/ private counter
      up to time_horizon with K arms of gap .1. Return the history up to time_horizon.
     """
+    means = get_means(gap)
+    K = len(means)
+    priv_noises = private_counter(K, time_horizon, epsilon, sensitivity=2)
+    # history at time 0
+    history = dict((i, [0, 0]) for i in range(K))
+    t = 1
+    # Sample initial point from each arm
+    while t <= K:
+        history[t-1] = [get_sample(means[t-1]), 1]
+        t += 1
+    # Run UCB Algorithm from t = K + 1 to t = time_horizon
+    while t <= time_horizon:
+        arm_pull = get_priv_ucb(delta, history, priv_noises, time_horizon, epsilon)
+        history = update_history(history, arm_pull, means)
+        t += 1
+    return history
 
 
 #
@@ -136,4 +164,20 @@ bias = map(add, mus, np.multiply(-1.0, average_mu_hat))
 print(bias)
 #  95% conf. lower bound for the bias (Hoeffding Inequality)
 w = np.sqrt(-1*np.log(.975/2)/(2.0*n_sims))
+print('non-private bias: {}'.format(bias))
 print('confidence width for bias: {}'.format(w))
+
+# Private Version
+cum_mu_hat = [0]*K
+for j in range(n_sims):
+    H_T = priv_ucb_bandit_run(time_horizon=T, delta=.95, gap=.1, epsilon=.1)
+    mu_hat = [H_T[i][0]/H_T[i][1] for i in range(K)]
+    cum_mu_hat = map(add, cum_mu_hat, mu_hat)
+
+# Compute the bias.
+average_mu_hat = np.multiply(1.0/n_sims, cum_mu_hat)
+priv_bias = map(add, mus, np.multiply(-1.0, average_mu_hat))
+print('private bias: {}'.format(priv_bias))
+#  95% conf. lower bound for the bias (Hoeffding Inequality)
+w_priv = np.sqrt(-1*np.log(.975/2)/(2.0*n_sims))
+print('confidence width for bias: {}'.format(w_priv))
